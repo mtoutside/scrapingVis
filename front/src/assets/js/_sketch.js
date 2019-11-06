6992c0b01194ed9104b8934bfffda781d2a20637
mtoutside <more.humor.or@gmail.com>
2019-11-07 01:52:35 +0900

    wip

--- front/src/assets/js/_sketch.js
+++ front/src/assets/js/_sketch.js
@@ -1,16 +1,33 @@
 
 export default function sketch(p) {
+  let data;
+  let count = 0;
+
+  p.preload = () => {
+    let url = 'http://localhost:3000';
+    data = p.loadJSON(url);
+  }
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.colorMode(p.HSB, 360, 100, 100);
-    p.background(255);
+    p.colorMode(p.HSB, 360, 100, 100, 100);
+    p.background(360);
+    p.cursor(p.CROSS);
+    p.frameRate(5);
   };
 
-  p.draw = () => {
-    p.background(255);
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
 
-    let hue = p.map(p.mouseX, 0, p.width, 0, 360);
-    p.fill(hue, 80, 80);
-    p.ellipse(p.mouseX, p.mouseY, 200, 200);
+  }
+  p.draw = () => {
+    p.background(360, 0, 100, 10);
+    // let str = data[p.int(p.random(Object.keys(data).length))];
+    let str = data[count];
+    p.textSize(p.random(14, 22));
+    p.text(str, p.mouseX, p.mouseY);
+    count++;
+
+    if(count >= data.length) count = 0;
+
   };
 }
